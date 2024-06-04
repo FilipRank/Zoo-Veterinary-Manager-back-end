82e89e11f71b3b96eead42abcebf9b076b15b176
Filip Kristijan Rankovic Singidunum <filip.kristijan.rankovic.22@singimail.rs>
2024-06-04 18:42:39 +0200

    Do many things

--- src/main/java/rs/nemamvebsajt/spring_test_project_2/model/Task.java
+++ src/main/java/rs/nemamvebsajt/spring_test_project_2/model/Task.java
@@ -1,6 +1,12 @@
 package rs.nemamvebsajt.spring_test_project_2.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.InstantDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
